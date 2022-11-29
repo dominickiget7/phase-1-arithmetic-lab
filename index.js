a74f5ab9ce75bbaa6f8f6e80e57c0f46d933252e
dominickiget7 <dominickiget7@gmail.com>
2022-11-29 08:53:27 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+const num1 = 2; const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10);
+const num3 = 25;
+const num4 = 21;
+const mod = num3 % num4;
+const max = Math.max(1, 4, 19, 16, 20)
